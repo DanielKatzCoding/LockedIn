d57febc6b4adb3999c5f04bf8b83a112bf6838d7
Daniel Katz <katzz.daniel@gmail.com>
2026-03-12 12:33:53 +0200

    added new model, patched id misstype

--- backend/api/applications.py
+++ backend/api/applications.py
@@ -3,6 +3,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from typing import List
 from services.database_service import db_service
 from backend.orm.schema import JobApplication
+from backend.model.job_application import CreateJobApplication
 
 router = APIRouter(prefix="/api/applications", tags=["applications"])
 
@@ -13,7 +14,7 @@ async def get_job_applications(skip: int = 0, limit: int = 100, db: AsyncSession
     return job_applications
 
 @router.post("/create", response_model=JobApplication, status_code=status.HTTP_201_CREATED)
-async def create_new_job_application(job_application: JobApplication, db: AsyncSession = Depends(db_service.get_db)):
+async def create_new_job_application(job_application: CreateJobApplication, db: AsyncSession = Depends(db_service.get_db)):
     return await db_service.crud.create_job_application(db=db, job_application_data=job_application.model_dump())
 
 @router.post("/create/empty", response_model=JobApplication, status_code=status.HTTP_201_CREATED)
@@ -22,7 +23,7 @@ async def create_new_empty_job_application(db: AsyncSession = Depends(db_service
 
 
 @router.get("/{job_application_id}", response_model=JobApplication)
-async def read_job_application(job_application_id: int, db: AsyncSession = Depends(db_service.get_db)):
+async def read_job_application(job_application_id: str, db: AsyncSession = Depends(db_service.get_db)):
     db_job_application = await db_service.crud.get_job_application(db, job_application_id=job_application_id)
     if db_job_application is None:
         raise HTTPException(status_code=404, detail="Job application not found")
@@ -31,7 +32,7 @@ async def read_job_application(job_application_id: int, db: AsyncSession = Depen
 
 @router.put("/{job_application_id}", response_model=JobApplication)
 async def update_existing_job_application(
-    job_application_id: int,
+    job_application_id: str,
     job_application: JobApplication,
     db: AsyncSession = Depends(db_service.get_db)
 ):
@@ -46,7 +47,7 @@ async def update_existing_job_application(
 
 
 @router.delete("/{job_application_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_existing_job_application(job_application_id: int, db: AsyncSession = Depends(db_service.get_db)):
+async def delete_existing_job_application(job_application_id: str, db: AsyncSession = Depends(db_service.get_db)):
     success = await db_service.crud.delete_job_application(db, job_application_id=job_application_id)
     if not success:
         raise HTTPException(status_code=404, detail="Job application not found")
